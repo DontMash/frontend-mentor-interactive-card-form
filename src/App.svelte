30590db02575506619da0db41ce4afc327e6b908
Sören Maschmann <smaschmann@slashwhy.de>
2022-10-29 18:30:19 +0200

    Initial

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<main>
+  0000 0000 0000 0000
+  Jane Appleseed
+  00/00
+
+  000
+
+  Cardholder Name
+  e.g. Jane Appleseed
+
+  Card Number
+  e.g. 1234 5678 9123 0000
+
+  Exp. Date (MM/YY)
+  MM
+  YY
+
+  CVC
+  e.g. 123
+
+  Confirm
+
+  <!-- Completed state start -->
+
+  Thank you!
+  We've added your card details
+  Continue
+  
+  <div>
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+    Coded by <a href="https://github.com/DontMash" target="_blank" rel="noreferrer">DontMash</a>.
+  </div>
+</main>
